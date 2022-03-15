6223f47519cfc9369f6741efcf4d474ecdb5c7a8
Mahlet Berhane <94903341+Ledant@users.noreply.github.com>
2022-03-15 14:57:29 -0500

    Beneficiary component and service added

---
+++ frontEnd/src/app/grant-dashboard/grant-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grant-dashboard',
+  templateUrl: './grant-dashboard.component.html',
+  styleUrls: ['./grant-dashboard.component.css']
+})
+export class GrantDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
