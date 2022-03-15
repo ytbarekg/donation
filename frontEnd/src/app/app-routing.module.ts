b92e85372d108bd33c7b2032f3c0c9b8e782f195
ytbarekg <yhailu@miu.edu>
2022-03-15 15:35:38 -0500

    added beneficiary component to routing

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './auth-guard.service';
+import { BeneficiaryRegistrationComponent } from './beneficiary-registration/beneficiary-registration.component';
 import { DonorDashboardComponent } from './donor-dashboard/donor-dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'beneficiary', component: BeneficiaryRegistrationComponent},
   {path: 'admin', data:{role: 'Admin'}, loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
   {path: 'donor', component: DonorDashboardComponent, data:{role: 'Donor'}, canActivate: [AuthGuardService]},
   {path: 'maker', component: MakerDashboardComponent, data:{role: 'Maker'}, canActivate: [AuthGuardService]},
